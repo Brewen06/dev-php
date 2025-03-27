1015250856ec1af39533bfb3d7d4c564ba7f313d
FROTIN Brewen <frotbre@ndlp.fr>
2025-03-27 11:05:57 +0100

    first commit

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+    // TODO : Ecrire dans fichier dev-php.log "Démarrage du programme"
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Petit projet pour réviser les bases</title>
+</head>
+<body>
+    <h1>Petit projet pour réviser les bases</h1>
+</body>
+</html>
